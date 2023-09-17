e55191fa498216a001dff0ab42c53540dc632f84
repour <poorsadeghi.r@gmail.com>
2023-09-17 22:52:41 +0330

    task2 js

---
+++ Part two/JS/02/main.js
@@ -0,0 +1,16 @@
+import './style.css'
+
+let givenStr = prompt("please enter your string");
+const givenArryOfSymbol = prompt("please enter your symboles , seperated with comma")
+
+function convertToHour(str , arry) {
+  str = str.trimEnd();
+
+  for (let i = 0; i < arry.length; i++) {
+    const element = array[i];
+    
+  }
+
+}
+convertToHour(givenNumb);
+console.log(convertToHour(givenNumb));
